2e569965131700bf748f25b0e2a76dc93f0a9bf1
Tesolite <pavel.kotka@gmail.com>
2022-11-27 21:40:54 +0000

    old code removed, better formatting, game can't continue after loss.

--- scripts/coding.js
+++ scripts/coding.js
@@ -85,48 +85,15 @@ async function gameRound() {
     return round_flag
 }
 
-/*document.onkeydown = function (verify) {
-    if ((verify.key == "ArrowLeft" && word == colour)
-        || (verify.key == "ArrowRight" && word != colour)) {
-        console.log("Correct!");
-        round_flag = 1;
-    }
-    else if ((verify.key == "ArrowLeft" && word != colour)
-        || (verify.key == "ArrowRight" && word == colour)) {
-        console.log("Incorrect!");
-        round_flag = 0;
-    }
-}
-return round_flag;
-*/
 
 function createRound() {
     var word = randWord();
     var colour = randColour();
-    //console.log(word + " and " + colour);
     document.getElementById("main").innerHTML = word;
     document.getElementById("main").style.color = colour;
 
     return [word, colour];
 }
-
-/*function verifyRound(input, word, colour) {
-    var verifiedFlag = 0;
-    console.log(word + " and " + colour);
-    if ((input == "j" && word == colour)
-        || (input == "k" && word != colour)) {
-        console.log("Correct!");
-        verifiedFlag = 1;
-    }
-    else if ((input == "j" && word != colour)
-        || (input == "k" && word == colour)) {
-        console.log("Incorrect!");
-        verifiedFlag = 0;
-    }
-
-    alert(verifiedFlag);
-    return verifiedFlag;
-}*/
 
 function verifyRound(input, round) {
     var word = round[0];
@@ -151,15 +118,29 @@ function startGame() {
     var word = round[0];
     var colour = round[1];
     score = 0
-    var continueGame = 1;
     console.log(word + colour);
-    document.addEventListener("keydown", function (input) {
-        if (verifyRound(input.key, round) == true){
-            score++;
-            round = createRound();
-            word = round[0];
-            colour = round[1];
-        }
-        else(alert("GAME OVER, FINAL SCORE: " + score));
-        }, true)
+    var continueGame = 1;
+    if (continueGame == 1) {
+        document.addEventListener("keydown", function registerInput(input) {
+            if (input.key == "ArrowLeft" || input.key == "ArrowRight") {
+                if (verifyRound(input.key, round) == true) {
+                    score++;
+                    round = createRound();
+                    word = round[0];
+                    colour = round[1];
+                }
+                else {
+                    alert("GAME OVER, FINAL SCORE: " + score);
+                    continueGame = 0;
+                    word = "GAME OVER";
+                    colour = "red";
+                    score = 0;
+                    document.removeEventListener("keydown", registerInput, true)
+                }
+            }
+        }, true);
+    }
+    else {
+        
+    }
 }
