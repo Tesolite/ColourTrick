2999fcb99f7e23498fff6cde38cb29bf76ecc294
Tesolite <pavel.kotka@gmail.com>
2022-11-27 17:05:57 +0000

    Working word + colour generator created, match verifier added

--- scripts/coding.js
+++ scripts/coding.js
@@ -1,5 +1,4 @@
-
-function randColour(){
+function randColour(){  
     var random = Math.floor(Math.random() * 6);
     colour = "";
 
@@ -21,5 +20,49 @@ function randColour(){
     else if(random == 5){
         colour = "purple";
     }
-    document.getElementById("main").style.color = colour;
+    return colour;
+}
+
+function randWord(){
+    var random = Math.floor(Math.random() * 6);
+    word = "";
+
+    if(random == 0){
+        word = "red";
+    }
+    else if(random == 1){
+        word = "orange";
+    }
+    else if(random == 2){
+        word = "yellow";
+    }
+    else if(random == 3){
+        word = "green";
+    }
+    else if(random == 4){
+        word = "blue";
+    }
+    else if(random == 5){
+        word = "purple";
+    }
+    return word;
+}
+function startGame(){
+    do{
+        var word = randWord();
+        var colour = randColour();
+
+        game_flag = 1;
+        console.log(word + " and " + colour);
+        document.getElementById("main").innerHTML = word;
+        document.getElementById("main").style.color = colour;
+        if(word == colour){
+            alert("Match!");
+            game_flag = 0;
+        }
+        else{
+            alert("Miss!");
+            game_flag = 0;
+        }
+    } while(game_flag != 0);
 }
